ce6f1bba8a8e9e51170e7c60885806bc67997036
Hung-Chen Fu <hung-chen.fu@sap.com>
2025-07-25 15:49:52 +0200

    feat: update payment components and add CheckoutForm for Stripe integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import "./App.css";
 import PaymentForm from "./PaymentForm";
+import CheckoutForm from "./StripeCheckoutForm";
+import { CheckoutProvider } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(import.meta.env.VITE_PUBLISHABLE_KEY);
+
+const fetchClientSecret = () => {
+  return fetch(`${import.meta.env.VITE_LAMBDA_BASE_PATH}/payment-api`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((json) => json.checkoutSessionClientSecret);
+};
 
 function App() {
-  return <PaymentForm />;
+  return (
+    <>
+      {/* <PaymentForm /> */}
+      <CheckoutProvider stripe={stripePromise} options={{ fetchClientSecret }}>
+        <CheckoutForm />
+      </CheckoutProvider>
+    </>
+  );
 }
 
 export default App;
